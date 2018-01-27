07fa63262c8880b3ad6f8cf499c77d17722a2e8f
Chris <chris_skart@hotmail.com>
2018-01-27 15:36:13 +0200

    Route changes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,97 @@
 var path = require('path');
+var express = express.Router();
+var Twitter = require('twitter');
+const multer = require('multer');
 
 module.exports = function(app, passport) {
+// Image upload ================================================================
+
+	const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 1000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('myImage');
+
+// Check File Type
+ function checkFileType(file, cb){
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif|mp4/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Images and Videos Only!');
+  }
+}
+
+
+// Public Folder
+router.use(express.static('./public'));
+
+app.get('/', (req, res) => res.render('index'));
+var data;
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      res.render('index', {
+        msg: err
+      });
+    } else {
+      if(req.file == undefined){
+        res.render('index', {
+          msg: 'Error: No File Selected!'
+        });
+      } else {
+      	data = require('fs').readFileSync(req.file.path);
+
+
+
+
+      	client.post('media/upload', {media: data}, function(error, media, response) {
+
+  if (!error) {
+
+    // If successful, a media object will be returned.
+    console.log(media);
+
+    // Lets tweet it
+    var status = {
+      status: 'I am a tweet',
+      media_ids: media.media_id_string // Pass the media id string
+    }
+
+    client.post('statuses/update', status, function(error, tweet, response) {
+      if (!error) {
+        console.log(tweet);
+      }
+    });
+
+  }
+});
+        res.render('index', {
+          msg: 'File Uploaded!',
+          file: `uploads/${req.file.filename}`
+        });
+        
+      }
+    }
+  });
+});
+
 
 
 // normal routes ===============================================================
