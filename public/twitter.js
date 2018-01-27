aa2c994deeb51deefde1f75e8313ece6d7940b31
Chris <chris_skart@hotmail.com>
2018-01-27 15:47:34 +0200

    major changes

--- public/twitter.js
+++ public/twitter.js
@@ -1,121 +1,76 @@
-var express = require('express');
-var router = express.Router();
-var Twitter = require('twitter');
-const multer = require('multer');
-const path = require('path');
-
-
-//Twitter credentials
-var client = new Twitter({
+const Twitter     = require('twitter');
+const client      = new Twitter({ 
   consumer_key: 'LLPwgnBjYUvpGusXyyQb5QMAh',
   consumer_secret: 'oCg6nwCy2HN3k1RgbP71xxLa8UQOEclKCFCrdnSbXLhO7KrnwG',
   access_token_key: '162027406-utH1pSRECdzsFzFaaPI1ekeHmwKPGEvbcw3Tqqo6',
-  access_token_secret: 'IfDKKJyziu87ZhQWFoBW9l3ttoqWvDIT9t75Sn4qvmrOq'
-});
+  access_token_secret: 'IfDKKJyziu87ZhQWFoBW9l3ttoqWvDIT9t75Sn4qvmrOq' });
 
-var params = {screen_name: 'nodejs'};
+const pathToMovie = '/path/to/your/video/animated-gif.gif';
+const mediaType   = 'image/gif'; // `'video/mp4'` is also supported
+const mediaData   = require('fs').readFileSync(pathToMovie);
+const mediaSize    = require('fs').statSync(pathToMovie).size;
 
-
-
-
-// Get Homepage
-router.get('/', ensureAuthenticated, function(req, res){
-  res.render('index');
-});
-
-function ensureAuthenticated(req, res, next){
-  if(req.isAuthenticated()){
-    return next();
-  } else {
-    //req.flash('error_msg','You are not logged in');
-    res.redirect('/views/index');
-  }
-}
-
-
-
-
-// Set The Storage Engine
-const storage = multer.diskStorage({
-  destination: './public/uploads/',
-  filename: function(req, file, cb){
-    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
-
-// Init Upload
-const upload = multer({
-  storage: storage,
-  limits:{fileSize: 1000000},
-  fileFilter: function(req, file, cb){
-    checkFileType(file, cb);
-  }
-}).single('myImage');
-
-// Check File Type
-function checkFileType(file, cb){
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif|mp4/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
-
-  if(mimetype && extname){
-    return cb(null,true);
-  } else {
-    cb('Error: Images and Videos Only!');
-  }
-}
-
-
-// Public Folder
-router.use(express.static('./public'));
-
-router.get('/', (req, res) => res.render('index'));
-var data;
-router.post('/upload', (req, res) => {
-  upload(req, res, (err) => {
-    if(err){
-      res.render('index', {
-        msg: err
-      });
-    } else {
-      if(req.file == undefined){
-        res.render('index', {
-          msg: 'Error: No File Selected!'
-        });
-      } else {
-        data = require('fs').readFileSync(req.file.path);
-        client.post('media/upload', {media: data}, function(error, media, response) {
-
-  if (!error) {
-
-    // If successful, a media object will be returned.
-    console.log(media);
-
-    // Lets tweet it
-    var status = {
-      status: 'I am a tweet',
-      media_ids: media.media_id_string // Pass the media id string
-    }
-
-    client.post('statuses/update', status, function(error, tweet, response) {
-      if (!error) {
-        console.log(tweet);
-      }
-    });
-
-  }
-});
-        res.render('index', {
-          msg: 'File Uploaded!',
-          file: `uploads/${req.file.filename}`
-        });
-        
-      }
-    }
+initUpload() // Declare that you wish to upload some media
+  .then(appendUpload) // Send the data for the media
+  .then(finalizeUpload) // Declare that you are done uploading chunks
+  .then(mediaId => {
+    // You now have an uploaded movie/animated gif
+    // that you can reference in Tweets, e.g. `update/statuses`
+    // will take a `mediaIds` param.
   });
-});
 
-module.exports = router;
+  /**
+   * Step 1 of 3: Initialize a media upload
+   * @return Promise resolving to String mediaId
+   */
+  function initUpload () {
+    return makePost('media/upload', {
+      command    : 'INIT',
+      total_bytes: mediaSize,
+      media_type : mediaType,
+    }).then(data => data.media_id_string);
+  }
+
+  /**
+   * Step 2 of 3: Append file chunk
+   * @param String mediaId    Reference to media object being uploaded
+   * @return Promise resolving to String mediaId (for chaining)
+   */
+  function appendUpload (mediaId) {
+    return makePost('media/upload', {
+      command      : 'APPEND',
+      media_id     : mediaId,
+      media        : mediaData,
+      segment_index: 0
+    }).then(data => mediaId);
+  }
+
+  /**
+   * Step 3 of 3: Finalize upload
+   * @param String mediaId   Reference to media
+   * @return Promise resolving to mediaId (for chaining)
+   */
+  function finalizeUpload (mediaId) {
+    return makePost('media/upload', {
+      command : 'FINALIZE',
+      media_id: mediaId
+    }).then(data => mediaId);
+  }
+
+  /**
+   * (Utility function) Send a POST request to the Twitter API
+   * @param String endpoint  e.g. 'statuses/upload'
+   * @param Object params    Params object to send
+   * @return Promise         Rejects if response is error
+   */
+  function makePost (endpoint, params) {
+    return new Promise((resolve, reject) => {
+      client.post(endpoint, params, (error, data, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
