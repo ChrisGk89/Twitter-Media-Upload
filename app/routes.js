64efdb1e92999d127df2d1186f1ee9501b5db372
Chris <chris_skart@hotmail.com>
2018-01-27 16:09:36 +0200

    changes in routes

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,9 @@
 var path = require('path');
+var Twitter = require('twitter');
+const multer = require('multer');
+var express = require('express');
+
+
 
 module.exports = function(app, passport) {
 
@@ -54,7 +59,6 @@ module.exports = function(app, passport) {
 			failureRedirect : '/signup', // redirect back to the signup page if there is an error
 			failureFlash : true // allow flash messages
 		}));
-
 	
 
 	// twitter --------------------------------
@@ -70,8 +74,6 @@ module.exports = function(app, passport) {
 			}));
 
 
-
-
 };
 
 // route middleware to ensure user is logged in
@@ -81,3 +83,54 @@ function isLoggedIn(req, res, next) {
 
 	res.redirect('/');
 }
+
+
+
+var data;
+app.post('/upload', function (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      res.render('index.ejs', {
+        message: req.flash(err)
+      });
+    } else {
+      if(req.file == undefined){
+        res.render('index.ejs', {
+          message: req.flash('Error: No File Selected!')
+        });
+      } else {
+      	data = require('fs').readFileSync(req.file.path);
+
+
+      	client.post('media/upload', {media: data}, function(error, media, response) {
+
+  if (!error) {
+
+    // If successful, a media object will be returned.
+    console.log(media);
+
+    // Lets tweet it
+    var status = {
+      status: 'I am a tweet',
+      media_ids: media.media_id_string // Pass the media id string
+    }
+
+    client.post('statuses/update', status, function(error, tweet, response) {
+      if (!error) {
+        console.log(tweet);
+      }
+    });
+
+  }
+});
+        res.render('index', {
+          msg: 'File Uploaded!',
+          file: `uploads/${req.file.filename}`
+        });
+        
+      }
+    }
+  });
+});
+
+
